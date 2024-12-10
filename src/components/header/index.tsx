d8159221ee6b6ebf2cb913c3e7c6a0abf3b6d2c7
zeroeuni <pkhj286@gmail.com>
2024-12-10 09:06:11 +0900

    chore: 설정 수정

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import * as S from "./style";
-import Logo from "../../assets";
+import Logo from "../../assets/TraviewLogo.svg";
 
 export const Header = () => {
   return (
     <S.HeaderLayout>
-      <S.Logo src={Logo} alt="Traview Logo" />
       <S.SearchBarWrapper>
         <S.SearchInput type="text" placeholder="검색어를 입력하세요..." />
         <S.SearchButton>검색</S.SearchButton>
